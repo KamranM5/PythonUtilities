de2cf223adf0be89012f8ba9eedd3fc6178508fa
KamranM5 <81294709+KamranM5@users.noreply.github.com>
2023-08-16 20:52:53 +0300

    Update videoFromYoutubeDownloader.py

--- videoFromYoutubeDownloader.py
+++ videoFromYoutubeDownloader.py
@@ -1,8 +1,8 @@
 from pytube import YouTube
 
-link=input('Введи ссылку')
+link=input('Enter the link:')
 
 url=YouTube(link)
 video=url.streams.get_highest_resolution()
 video.download()
-print('Done(готово)...')
+print('Done!')
